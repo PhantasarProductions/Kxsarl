43d38d93a9fef6e5c624a96ffaeb0b3589f369bc
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-07-19 17:07:30 +0200

    Import

--- ba.ps1
+++ ba.ps1
@@ -1,1 +1,3 @@
+cp "Scyndi:\Projects\Fonts\JFBF\Ryanna.jfbf" "Assets/Jeroen P. Broks/Fonts/"
+cp "Scyndi:\Projects\Fonts\JFBF\Mini.jfbf" "Assets/Jeroen P. Broks/Fonts/"
 scyndi:\Projects\Applications\Slyvina\Apps\Dir2JQL\EXE\dir2jql E:/Projects/Applications/Slyvina/Apps/Kxsarl/Assets
