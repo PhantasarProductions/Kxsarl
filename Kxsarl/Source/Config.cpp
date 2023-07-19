7518b7b04a5096045a21b7beda7dadd417c09b96
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-07-19 17:08:21 +0200

    Full Screen and Windowed Screen can not be set

--- Kxsarl/Source/Config.cpp
+++ Kxsarl/Source/Config.cpp
@@ -31,13 +31,17 @@
 #include <SlyvTime.hpp>
 
 #include <TQSG.hpp>
+#include <TQSE.hpp>
 
+#include "../Headers/GlobGFX.hpp"
 #include "../Headers/Config.hpp"
+#include "../Headers/UseJCR6.hpp"
 
 
 using namespace Slyvina;
 using namespace Units;
 using namespace TQSG;
+using namespace TQSE;
 
 namespace Kxsarl {
 
@@ -83,12 +87,41 @@ namespace Kxsarl {
 	}
 	bool CFG_Windowed() {
 		DefaultScreen();
-		return Upper(Config()->Value("Screen", "Windowed")) == "YES";
+		auto c{ Upper(Config()->Value("Screen", "Windowed")) };
+		return c == "YES" || c == "TRUE";
 	}
 	std::string CFG_ScreenModeString() {
 		if (CFG_Windowed())
 			return TrSPrintF("%d x %d (Windowed)", CFG_Width(), CFG_Height());
 		else
 		return TrSPrintF("%d x %d (FullScreen)", DesktopWidth(),DesktopHeight());
+	}	
+
+	void Arrive_Config() {
+#ifdef KXSARL_DEBUG
+		//for (auto j : MRes()->_Entries) std::cout << j.first << " -> " << j.second->Name() << "\n";
+#endif
+	}
+
+	bool Flow_Config() {
+		static TUImage Background{nullptr}; if (!Background) Background = LoadUImage(MRes(),"GFX/Config/comp024.jpg");
+		static TUImage Banner{nullptr}; if (!Banner) Banner = LoadUImage(MRes(),"GFX/Banner/Config.png");
+		SetColor(127,127,127);
+		Background->Tile(0, 0, ScreenWidth(), ScreenHeight());
+		SetColor(255, 255, 255);
+		Banner->StretchDraw(0, 0, ScreenWidth(), 104);
+		SetColor(0, 180, 255);
+		Ryanna()->Text("Screen settings:", 20, 120); //printf("H: %9d\n", Ryanna()->Height("ABCabc")); 
+		SetColor(255, 0, 0); MiniFont()->Text("NOTE!", 20, 150); SetColor(255, 255, 255);
+		MiniFont()->Text("The base screen setting is 1900x1000. Any other setting is allowed, but could \"misform\" the graphics a bit", 20, 165);
+		MiniFont()->Text("Also note that these settings only take effect during the game's main executable start up.", 20, 180);
+		MiniFont()->Text("In other words, you'll need to exit the game and start the game again in order for these to take effect!", 20, 195);
+		CheckBox(!CFG_Windowed())->StretchDraw(20, 210, 30, 30); Ryanna()->Text("Full Screen",60, 210);
+		//QCol->Reset();  printf("Mouse(%d,%d); Start(%d,%d); End(%d,%d); Click: %s\n", MouseX(), MouseY(), ASX(20), ASY(210), ASX(50), ASY(240), boolstring(MouseHit(1)).c_str()); // debug
+		if (MouseX() > ASX(20) && MouseX() < ASX(50) && MouseY() > ASY(210) && MouseY() < ASY(240) && MouseHit(1)) {
+			Config()->Value("Screen", "Windowed", boolstring(!CFG_Windowed()));
+			CSay("Windowed = " + boolstring(CFG_Windowed()));
+		}
+		return true;
 	}
 }
