84a87d2cb5cc351003c3b26154aeaa4c981afa85
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-07-28 20:07:16 +0200

    Prepare to fake it all

---
+++ Textures/FakeItAll/FakeItAll.cpp
@@ -0,0 +1,37 @@
+// Lic:
+// Fake It All
+// .
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2023
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 23.07.28
+// EndLic
+
+// Please note that FakeItAll (and the Blitz Basic program Fake 3D)
+// Are NOT part of the game distribution, but merely some tools meant for
+// development purposes during the projects design stages only. 
+
+// I added it to this repository for examination purposes, but that's all.
+// You can also expect a kind of "dirty" way to access directories and stuff
+// as a result.
+
+int main(int ac, char** a) {
+
+}
