817550f81e12dbe7999e418481e98af07a694062
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-08-02 12:52:57 +0200

    Prep for #1 Transfer program

---
+++ Kxsarl_Transfer/Form1.cs
@@ -0,0 +1,7 @@
+namespace Kxsarl_Transfer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ cs2m.ps1
@@ -0,0 +1,1 @@
+cp E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64\Release\net6.0-windows\* E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64
