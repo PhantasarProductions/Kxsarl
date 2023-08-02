99519ca2e21b898c510121c53dd7414375b0fe24
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-08-02 17:09:29 +0200

    Transfer workout

--- Kxsarl_Transfer/MainWindow.xaml.cs
+++ Kxsarl_Transfer/MainWindow.xaml.cs
@@ -1,4 +1,29 @@
-﻿using System;
+// Lic:
+// The Legend of the Kxsarl
+// Transfer Main Window (code)
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2023
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 23.08.02
+// EndLic
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +37,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TrickyUnits;
 
 namespace Kxsarl_Transfer {
     /// <summary>
@@ -20,6 +46,15 @@ namespace Kxsarl_Transfer {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+        private void MayExport() {
+            // Code comes later!
+        }
+
+        private void Browse_ExportTo_Click(object sender, RoutedEventArgs e) {
+            var WantFile = FFS.RequestFile(true);
+            if (WantFile != "") TB_ExportTo.Text = WantFile;
+            MayExport();
         }
     }
 }
