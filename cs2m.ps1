cba33274cdf0bef79b9026958aa1a1fa0f3bd293
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-08-02 13:18:03 +0200

    A few setups

--- cs2m.ps1
+++ cs2m.ps1
@@ -1,1 +1,2 @@
-cp E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64\Release\net6.0-windows\* E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64
+#cp E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64\Release\net6.0-windows\* E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64
+cp E:\Projects\Applications\Slyvina\Apps\Kxsarl\Kxsarl_Transfer\Private.Bin\Release\* E:\Projects\Applications\Slyvina\Apps\Kxsarl\Exe_x64
